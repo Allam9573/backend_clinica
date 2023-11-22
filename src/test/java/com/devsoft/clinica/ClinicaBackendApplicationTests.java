9db5b752fd99dfbdb8d52a259047003bfff2e167
Allam9573 <aallamdaniel@gmail.com>
2023-11-22 14:13:07 -0600

    primer commit

---
+++ src/test/java/com/devsoft/clinica/ClinicaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsoft.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
