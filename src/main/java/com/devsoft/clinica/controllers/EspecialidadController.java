ab559437a544fdcb4bde40990f3566fd5439dff0
Allam9573 <aallamdaniel@gmail.com>
2023-12-03 21:08:58 -0600

    config http laravel

--- src/main/java/com/devsoft/clinica/controllers/EspecialidadController.java
+++ src/main/java/com/devsoft/clinica/controllers/EspecialidadController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.devsoft.clinica.models.Especialidad;
 import com.devsoft.clinica.services.impl.EspecialidadServiceImpl;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8000")
 @RequestMapping("/api/especialidades")
 public class EspecialidadController {
 
