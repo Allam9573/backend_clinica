af5209bfe1bfea933871e9cd867fa802e6ba47a0
Allam9573 <aallamdaniel@gmail.com>
2023-12-04 14:07:20 -0600

    formateo

--- src/main/java/com/devsoft/clinica/services/EspecialidadService.java
+++ src/main/java/com/devsoft/clinica/services/EspecialidadService.java
@@ -7,9 +7,13 @@ import com.devsoft.clinica.models.Especialidad;
 public interface EspecialidadService {
 
     public Especialidad crearEspecialidad(Especialidad especialidad);
+
     public List<Especialidad> listarEspecialidades();
+
     public String eliminarEspecialidadPorId(int id);
+
     public Especialidad actualizarEspecialidad(int id, Especialidad especialidad);
+
     public Especialidad buscarEspecialidad(int id);
-    
+
 }
